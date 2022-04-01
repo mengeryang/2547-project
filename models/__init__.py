e9e0208581fbb72e53e09f41543baa0b5e5dafa1
wren93 <wren@cs.toronto.edu>
2022-04-01 15:55:23 -0400

    add auto loss weighing

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,7 @@
-from .errnet_model import ERRNetModel
+from .errnet_model import ERRNetModel, ERRNetALWModel
 
 def errnet_model():
     return ERRNetModel()
+
+def errnet_alw_model():
+    return ERRNetALWModel()
